5fd62d96e393bc941c60243290597629990df449
redusearhc <redusearhc@gmail.com>
2025-04-24 09:10:44 -0400

    Update main.py

--- redusearch/app/main.py
+++ redusearch/app/main.py
@@ -1,16 +1,7 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
+from fastapi import FastAPI
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
